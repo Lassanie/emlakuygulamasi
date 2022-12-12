c5825c42a0596aaab7b14670e6a3d9ce977a7efd
Xweser <internetruinedmylaifu@gmail.com>
2022-12-12 23:34:55 +0300

    First type

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = 
+{
+  reactStrictMode: true,
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
+}
+nextConfig
